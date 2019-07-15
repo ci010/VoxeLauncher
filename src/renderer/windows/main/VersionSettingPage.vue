e2a5045d8b031e402bb2cbf71d868874ccc1e76f
CI010 <cijhn@hotmail.com>
2019-07-15 13:54:07 +0800

    Optmize UX & release v0.2.0-beta.0 (#42)
    * refactor: add more log
    
    * feat: add more protocol support
    
    * feat: support server UX
    
    * fix: make status nonnull
    
    * fix(locale): add server text
    
    * chore: add parse wiki script
    
    * refactor(style): make profile setting as normal page
    
    * fix(ui): align the btn
    
    * fix: make the page swap more easy
    
    * feat: support hover to show percentage
    
    * fix(ui): highlight selected version
    
    * fix: prevent launch if use has change setting
    
    * refactor: move the diagnose to other place
    fix: reset launch state after editing profile
    
    * refactor: make diagnose standalone, lock the fixing problems
    
    * style: make the problem bar show during diagnose
    
    * fix(locale): add refresh skin text
    
    * fix: try use the selfhost for forge browsing
    
    * fix: wrong forge version save/load
    
    * fix: refresh forge logic
    
    * feat: support self host forge maven
    
    * fix: make percentage progress show on hover node
    
    * fix: security issue
    
    * fix: add missing pingserver func
    
    * refactor: add modify profile log
    
    * fix(ui): title align & tooltip
    
    * fix: commit the vm & mc options
    
    * fix: wrong loading server status;
    
    also add more log
    
    * fix(ui): some padding and text display
    
    * fix(locale): resourcepack & mod text
    
    * fix: forge refresh status;
    refactor: add more useful log;
    
    * fix: gamesetting & name profile assignment
    
    * refactor: don't wait to improve performance
    
    * fix: don't save server status
    
    * fix: try make the mc detach with launcher
    
    * fix(locale): add unknown server description
    
    * chore(release): 0.2.0-beta.0
    
    * style: try fix the lint warning

--- src/renderer/windows/main/VersionSettingPage.vue
+++ src/renderer/windows/main/VersionSettingPage.vue
@@ -7,11 +7,11 @@
       <v-flex xs6 style="margin-top: 10px;">
         <v-layout row align-end justify-end>
           <v-spacer />
-          <v-chip label dark outline style="transition: transform 1s;">
-            Minecraft: {{ mcversion }}
+          <v-chip color="primary" label dark outline style="transition: transform 1s;">
+            Minecraft:  {{ mcversion }}
           </v-chip>
           <v-expand-x-transition>
-            <v-chip v-show="forgeVersion !== ''" label dark outline style="white-space: nowrap">
+            <v-chip v-show="forgeVersion !== ''" color="brown" label dark outline style="white-space: nowrap">
               Forge:
               {{ forgeVersion }}
             </v-chip>
@@ -19,14 +19,14 @@
         </v-layout>
       </v-flex>
       <v-flex xs12>
-        <v-tabs v-model="active" mandatory color="transparent" dark slider-color="primary">
+        <v-tabs v-model="active" mandatory color="transparent" dark :slider-color="barColor">
           <v-tab>
             {{ $t('version.locals') }}
           </v-tab>
           <v-tab>
             Minecraft
           </v-tab>
-          <v-tab @click="refreshForgeVersion">
+          <v-tab @click="refreshForgeVersion(false)">
             Forge
           </v-tab>
           <v-tab>
@@ -37,7 +37,7 @@
         <v-tabs-items v-model="active" color="transparent" dark slider-color="primary" style="height: 70vh; overflow-y: auto"
                       @mousewheel="onMouseWheel">
           <v-tab-item style="height: 100%" @mousewheel="onMouseWheel">
-            <local-version-list :filter-text="filterText" @value="selectLocalVersion" />
+            <local-version-list :filter-text="filterText" :selected="localVersion" @value="selectLocalVersion" />
           </v-tab-item>
           <v-tab-item style="height: 100%" @mousewheel="onMouseWheel">
             <v-list-tile style="margin: 0px 0;">
@@ -46,7 +46,7 @@
             <v-divider dark />
 
             <minecraft-version-list :filter="filterMinecraft" style="background-color: transparent;"
-                                    :mcversion="mcversion" @value="mcversion = $event.id" />
+                                    :mcversion="mcversion" :selected="mcversion" @value="mcversion = $event.id" />
           </v-tab-item>
           <v-tab-item style="height: 100%" @mousewheel="onMouseWheel">
             <v-list-tile>
@@ -56,7 +56,7 @@
             </v-list-tile>
             <v-divider dark />
             <forge-version-list style="background-color: transparent" :refreshing="$repo.state.version.refreshingForge" :version-list="forgeVersionList"
-                                :mcversion="mcversion" :filter="filterForge" @refresh="refreshForgeVersion" @value="forgeVersion = $event ? $event.version : ''" />
+                                :mcversion="mcversion" :filter="filterForge" @refresh="refreshForgeVersion(true)" @value="forgeVersion = $event ? $event.version : ''" />
           </v-tab-item>
           <v-tab-item style="height: 100%" @mousewheel="onMouseWheel">
             <liteloader-version-list :mcversion="mcversion" :filter-text="filterText" @value="liteloaderVersion = $event ? $event.version : ''" />
@@ -68,10 +68,8 @@
 </template>
 
 <script>
-import AbstractSetting from '../mixin/AbstractSetting';
-
 export default {
-  mixins: [AbstractSetting],
+  mixins: [],
   data() {
     return {
       active: 0,
@@ -91,6 +89,16 @@ export default {
     };
   },
   computed: {
+    barColor() {
+      switch (this.active) {
+        case 0: return 'white';
+        case 1: return 'primary';
+        case 2: return 'brown';
+        case 3: return 'cyan';
+        default: return 'primary';
+      }
+    },
+    localVersion() { return { minecraft: this.mcversion, forge: this.forgeVersion, liteloader: this.liteloaderVersion }; },
     profile() { return this.$repo.getters.selectedProfile; },
   },
   watch: {
@@ -99,6 +107,10 @@ export default {
       this.liteloaderVersion = '';
     },
   },
+  mounted() { this.load(); },
+  destroyed() { this.save(); },
+  activated() { this.load(); },
+  deactivated() { this.save(); },
   methods: {
     save() {
       this.$repo.dispatch('editProfile', {
@@ -114,28 +126,34 @@ export default {
       this.forgeVersion = profile.forge.version;
 
       const mcversion = this.mcversion;
-      console.log(mcversion);
       const ver = this.$repo.state.version.forge[mcversion];
       if (ver) {
         this.forgeVersionList = ver.versions;
       } else {
+        this.forgeVersionList = [];
         this.$repo.dispatch('getForgeWebPage', this.mcversion)
           .then(r => (r ? r.versions : []))
-          .then((r) => { this.forgeVersionList = r; });
+          .then((r) => { this.forgeVersionList = [...r]; });
       }
     },
     onMouseWheel(e) {
       e.stopPropagation();
       return false;
     },
-    refreshForgeVersion() {
-      if (this.mcversion !== this.profile.mcversion) {
-        this.$repo.dispatch('getForgeWebPage', this.mcversion)
-          .then(r => (r ? r.versions : []))
-          .then((r) => { this.forgeVersionList = r; });
+    async refreshForgeVersion(force) {
+      if (force || this.mcversion !== this.profile.mcversion) {
+        this.forgeVersionList = [];
+        let r;
+        if (force) {
+          await this.$repo.dispatch('refreshForge', this.mcversion);
+          r = this.$repo.state.version.forge[this.mcversion];
+        } else {
+          r = await this.$repo.dispatch('getForgeWebPage', this.mcversion);
+        }
+        await this.$nextTick();
+        this.forgeVersionList = r ? Object.freeze([...r.versions]) : Object.freeze([]);
       }
     },
-
     filterMinecraft(v) {
       if (!this.showAlpha && v.type !== 'release') return false;
       return v.id.indexOf(this.filterText) !== -1;
@@ -162,9 +180,5 @@ export default {
 <style scoped=true>
 .flex {
   padding: 6px 8px !important;
-}
-.slide-x-transition {
-}
-.slide-x-transition {
 }
 </style>
